fcebc0c7760998c0dd2a2bb3044af0f6e165497f
aldo <aldohernandezcelestino@gmail.com>
2024-06-05 16:52:02 -0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '''
+        <!doctype html>
+        <html lang="es">
+          <head>
+            <meta charset="utf-8">
+            <style>
+              #imagen {
+                display: none;
+                width: 200px;
+                height: auto;
+              }
+            </style>
+          </head>
+          <body>
+            <h1>Hola mundo</h1>
+            <button onclick="mostrarMensaje()">Mostrar Mensaje</button>
+            <p id="mensaje"></p>
+            <img id="imagen" src="/static/imagen2.jpg" alt="Imagen de ejemplo">
+            <script>
+              function mostrarMensaje() {
+                fetch('/mensaje')
+                  .then(response => response.text())
+                  .then(data => {
+                    document.getElementById('mensaje').innerText = data;
+                    document.getElementById('imagen').style.display = 'block';
+                  });
+              }
+            </script>
+          </body>
+        </html>
+    '''
+
+@app.route('/mensaje')
+def mensaje():
+    return "Alumno: Aldo de Jesus Hernandez Celestino, Grupo 9°-B"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
